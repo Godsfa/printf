c2d2ac4b5eaf3dc48a9a3212466c83b17378877e
Godsfa <godsfavouromokhomion47@gmail.com>
2023-09-18 11:09:18 +0100

    commiting the task 0 file

---
+++ 1_output_format.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int printed_chars = 0;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            putchar(*format);
+            printed_chars++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                putchar(c);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    putchar(*str);
+                    str++;
+                    printed_chars++;
+                }
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                printed_chars++;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+    return printed_chars;
+}
--- main.c
+++ main.c
@@ -10,57 +10,6 @@
  *
  * Return: Number of characters printed
  */
-int _printf(const char *format, ...)
-{
-    va_list args;
-    int printed_chars = 0;
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            putchar(*format);
-            printed_chars++;
-        }
-        else
-        {
-            format++;
-            if (*format == '\0')
-                break;
-
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                putchar(c);
-                printed_chars++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    putchar(*str);
-                    str++;
-                    printed_chars++;
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                printed_chars++;
-            }
-           
-        }
-        format++;
-    }
-
-    va_end(args);
-    return printed_chars;
-}
 
 int main(void)
 {
