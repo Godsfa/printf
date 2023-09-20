35748aa733d2eb943b50d23e0fce6fe8af437968
Godsfa <godsfavouromokhomion47@gmail.com>
2023-09-20 16:55:47 +0100

    commit

--- 0-output_format.c
+++ 0-output_format.c
@@ -1,60 +1,60 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
+#include <unistd.h>
 
 /**
-* _printf - Custom printf function
-* @format: Format string
-*
-* Return: Number of characters printed (excluding the null byte)
-*/
+ * _printf - Custom printf function
+ * @format: Format string
+ * @...: Variable number of arguments
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-int printed_chars = 0;
+    va_list args;
+    int printed_chars = 0;
 
-va_start(args, format);
+    va_start(args, format);
 
-while (*format)
-{
-if (*format != '%')
-{
-putchar(*format);
-printed_chars++;
-}
-else
-{
-format++;
-if (*format == '\0')
-break;
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            printed_chars++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
 
-if (*format == 'c')
-{
-char c = va_arg(args, int);
-putchar(c);
-printed_chars++;
-}
-else if (*format == 's')
-{
-char *str = va_arg(args, char *);
-if (str == NULL)
-str = "(null)";
-while (*str)
-{
-putchar(*str);
-str++;
-printed_chars++;
-}
-}
-else if (*format == '%')
-{
-putchar('%');
-printed_chars++;
-}
-}
-format++;
-}
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    printed_chars++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                printed_chars++;
+            }
+        }
+        format++;
+    }
 
-va_end(args);
-return printed_chars;
+    va_end(args);
+    return printed_chars;
 }
